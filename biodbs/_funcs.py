c9497423f929546b150919165673045292ba2335
qwerty239qwe <qwerty239qwe@gmail.com>
2022-04-16 15:13:47 +0800

    add _func.py

---
+++ biodbs/_funcs.py
@@ -0,0 +1,8 @@
+from BioMart import Dataset
+
+
+def translate_genes(genes, from_id, to_id, **kwargs):
+    biom_ds = Dataset(**kwargs)
+    filter_kws = {to_id: genes}
+    result = biom_ds.get_data(attribs=[from_id], **filter_kws)
+    return result[to_id]
