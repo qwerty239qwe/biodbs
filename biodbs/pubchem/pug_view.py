9b1cc2c45ca5ccc49e594b31d4fd3bba5e937ed6
林祐德 <qwerty239qwe@gmail.com>
2024-01-27 19:44:07 +0800

    add several package level function files. fix pug_view

--- biodbs/pubchem/pug_view.py
+++ biodbs/pubchem/pug_view.py
@@ -1,5 +1,6 @@
 from biodbs.fetcher import BaseAPI
 from biodbs.utils import get_rsp
+from biodbs.pubchem.utils import parse_cmp, clean_value
 
 
 class PUGViewAPI(BaseAPI):
@@ -8,17 +9,26 @@ class PUGViewAPI(BaseAPI):
         self._url_format = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/{record_type}/{id}/{file_format}"
 
 
-class CompoundFetcher:
-    def __init__(self):
+class Fetcher:
+    def __init__(self, record_type):
         self._api = PUGViewAPI()
-        self._api.update_params(record_type="compound")
+        self._api.update_params(record_type=record_type)
 
-    def fetch(self, cid, file_format):
+    def fetch(self, cid, file_format) -> dict:
         fetched_api = self._api.apply(id=cid, file_format=file_format)
         resp = get_rsp(fetched_api.api)
         return resp.json()
 
-    def fetch_and_process(self, cid, file_format):
-        result = self.fetch(cid=cid, file_format=file_format)
+    def get_processed_data(self, cid):
+        result = self.fetch(cid=cid, file_format="JSON")
+        return clean_value(parse_cmp(result["Record"]["Section"]))
 
 
+class CompoundFetcher(Fetcher):
+    def __init__(self):
+        super().__init__("compound")
+
+
+class AssayFetcher(Fetcher):
+    def __init__(self):
+        super().__init__("assay")
--- biodbs/pubchem/utils.py
+++ biodbs/pubchem/utils.py
@@ -1,6 +1,7 @@
+from typing import Union
 
 
-def parse_cmp(cmp_dics: dict):
+def parse_cmp(cmp_dics: Union[list, dict]) -> dict:
     new_dic = {}
 
     for cmp_dic in cmp_dics:
@@ -15,4 +16,53 @@ def parse_cmp(cmp_dics: dict):
             new_dic[new_key] = new_val
     if len(new_dic) == 0:
         return cmp_dics
+    return new_dic
+
+
+def _find_vals(val_dic):
+    """
+    A helper function of clean_value()
+
+    :param val_dic:
+    :return:
+    """
+    for k, v in val_dic.items():
+        if isinstance(v, list):
+            if isinstance(v[0], dict):
+                return [val for vi in v for tp, val in vi.items() if tp not in ["Markup"]]
+            else:
+                return v
+        else:
+            return v
+
+
+def clean_value(cmp_dic):
+    """
+    Remove values with ReferenceNumber, Description, and DisplayControls keys,
+    flatten the values if it is a list with single value,
+    and remove the data type indicators in the given dict.
+
+    :param cmp_dic:
+    :return:
+    """
+    new_dic = {}
+    if isinstance(cmp_dic, list):
+        found_val = [clean_value(v) for v in cmp_dic]
+        return found_val if len(found_val) > 1 else found_val[0]
+
+    if not isinstance(cmp_dic, dict):
+        return cmp_dic
+
+    if "Value" in cmp_dic and "Name" in cmp_dic:
+        return {cmp_dic["Name"]: _find_vals(cmp_dic["Value"])}
+
+    for k, v in cmp_dic.items():
+
+        if k in ["ReferenceNumber", "Description", "DisplayControls"]:
+            continue
+        if k == "Value":
+            found_val = _find_vals(v)
+            return found_val[0] if (isinstance(found_val, list) and len(found_val) == 1) else found_val
+        else:
+            new_dic[k] = clean_value(v)
     return new_dic
