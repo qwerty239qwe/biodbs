64c593f0a87f52fd2e6e7681be5a70f6c4023494
林祐德 <qwerty239qwe@gmail.com>
2024-01-28 19:26:53 +0800

    add pug_rest's fetchers

--- biodbs/pubchem/pug_rest.py
+++ biodbs/pubchem/pug_rest.py
@@ -1,5 +1,6 @@
 from biodbs.fetcher import BaseAPI
 from biodbs.utils import get_rsp
+from biodbs.pubchem._params import *
 
 
 class PUGRestAPI(BaseAPI):
@@ -13,13 +14,73 @@ class Fetcher:
     def __init__(self, domain):
         self._api = PUGRestAPI()
         self._api.update_params(domain=domain)
+        self._valid_ns = PUBRESTNameSpace(domain=domain)
+        self._valid_ops = PUBRESTOperation(domain=domain)
 
-    def fetch(self, name_space, ids, operation, output_format="JSON"):
+        self._valid_params = {"namespace": self._valid_ns, "operation": self._valid_ops}
+
+    def _check_params(self, param, param_name):
+        return self._valid_params[param_name].match(param)
+
+    def fetch(self, name_space, ids, operation, output_format="JSON", operation_options=None):
         if isinstance(ids, list):
             ids = ",".join(ids)
 
+        if not self._check_params(name_space, "namespace"):
+            raise ValueError("namespace got an invalid value. Please choose from: "
+                             f"{self._valid_params['namespace'].name_space}")
+
+        if not self._check_params(operation, "operation"):
+            raise ValueError("namespace got an invalid value. Please choose from: "
+                             f"{self._valid_params['operation'].operations}")
+
+        operation_options = operation_options or ""
         fetched_api = self._api.apply(namespace=name_space,
                                       identifiers=ids,
                                       operation=operation,
                                       output_format=output_format,
+                                      operation_options=operation_options,
                                       )
+
+        resp = get_rsp(fetched_api.api)
+        return resp.json()
+
+
+class CompoundFetcher(Fetcher):
+    def __init__(self):
+        super().__init__(domain="compound")
+
+
+class SubstanceFetcher(Fetcher):
+    def __init__(self):
+        super().__init__(domain="substance")
+
+
+class AssayFetcher(Fetcher):
+    def __init__(self):
+        super().__init__(domain="assay")
+
+
+class GeneFetcher(Fetcher):
+    def __init__(self):
+        super().__init__(domain="gene")
+
+
+class ProteinFetcher(Fetcher):
+    def __init__(self):
+        super().__init__(domain="protein")
+
+
+class TaxonomyFetcher(Fetcher):
+    def __init__(self):
+        super().__init__(domain="taxonomy")
+
+
+class CellFetcher(Fetcher):
+    def __init__(self):
+        super().__init__(domain="cell")
+
+
+class OthersFetcher(Fetcher):
+    def __init__(self):
+        super().__init__(domain="others")
