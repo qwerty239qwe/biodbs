6803ded9dc8797dbf9eb6a1aa971bef58f233c4d
qwerty239qwe <qwerty239qwe@gmail.com>
2026-01-11 23:29:49 +0800

    add validate method to the FDANameSpace

--- biodbs/fetcher/FDA/fda_fetcher.py
+++ biodbs/fetcher/FDA/fda_fetcher.py
@@ -1,5 +1,7 @@
 from biodbs.fetcher import BaseAPI
-from enum import Enum
+from typing import Tuple
+
+
 
 class FDACategory:
     def __init__(self, name, endpoints):
@@ -16,13 +18,28 @@ class FDACategory:
     
 
 class FDANameSpace:
-    category = [FDACategory(name="drug", endpoints=["label"])]
+    all_categories = [FDACategory(name="drug", endpoints=["label", "event", "ndc"]),
+                      FDACategory(name="food", endpoints=["event", "enforcement"])]
+    
+    def validate(self, category, endpoint) -> Tuple[bool, str]:
+        if category not in [cate.name for cate in self.all_categories]:
+            return False, f"{category} is not a valid category."
+
+        for cate in self.all_categories:
+            if category == cate.name:
+                is_valid_endpoint = endpoint in cate.endpoints
+
+                return is_valid_endpoint, "" if is_valid_endpoint else f"{endpoint} is not a valid endpoint"
+        return False, f"{category} is not a valid category."
+        
 
 
 class FDA_API(BaseAPI):
     def __init__(self):
         super().__init__()
         self._url_format = "https://api.fda.gov/{category}/{endpoint}.json"
+        self.namespace = FDANameSpace()
+        
     
 
 class FDA_Fetcher:
